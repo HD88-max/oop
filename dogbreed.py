4300f27711103fb2daa7953ee6fabea8521290c2
HD88-max <hardikdhallam@gmail.com>
2025-04-10 10:48:27 +0100

    Add files via upload

---
+++ dogbreed.py
@@ -0,0 +1,9 @@
+class Dog:
+    def __init__(self,breed,colour):
+       self.breed=breed
+       self.colour=colour 
+
+dog1 = Dog("Cockapoo","Gold")
+dog2 = Dog("Retriever","Black")
+print(dog1.breed,dog1.colour)
+print(dog2.breed,dog2.colour)
